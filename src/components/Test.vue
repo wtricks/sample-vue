aca6f6a787ff8e552010163fb1c503b71e3b5031
wtricks <webtricks.ak@gmail.com>
2024-03-01 11:29:47 +0530

    Added "clear" and "capitilize" button for input data

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -5,6 +5,10 @@
 </script>
 
 <template>
-    <input type="text" placeholder="Enter your name" name="fname" v-model="name" />
     <h1>Hello {{name}}</h1>
+
+    <br>
+    <input type="text" placeholder="Enter your name" name="fname" v-model="name" />
+    <button type="button" title="Clear Name" @click="name = ''">Clear</button>
+    <button type="button" title="Clear Name" @click="name = name.toUpperCase()">Make capital</button>
 </template>
